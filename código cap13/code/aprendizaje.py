4b9ca5fdb199e275c0ca3e60dadffe1a9cd5379a
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2023-01-05 00:05:42 +0100

    cambios

--- código cap13/code/aprendizaje.py
+++ código cap13/code/aprendizaje.py
@@ -63,8 +63,68 @@ class Aprendizaje ():
         print("Etiqueta de la imagen N.° 70000...")
         print(Etiquetas[70000])
 
-    
 
+class validacion():
+
+    def __init__(self, Imagenes, Etiquetas, epochs, batch_size):
+        self.imagenes_aprendizaje, self.imagenes_validacion, self.etiquetas_aprendizaje, self.etiquetas_validacion = train_test_split(Imagenes, Etiquetas, test_size=0.25, random_state=42)
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+    def validacion(self, anchoimagen, largoimagen, cantidad_de_clases):
+        self.imagenes_aprendizaje = self.imagenes_aprendizaje.reshape(self.imagenes_aprendizaje.shape[0], anchoimagen, largoimagen, 1)
+        self.imagenes_validacion = self.imagenes_validacion.reshape(self.imagenes_validacion.shape[0], anchoimagen, largoimagen, 1)
+
+        self.imagenes_aprendizaje = self.imagenes_aprendizaje.astype('float32')
+        self.imagenes_validacion = self.imagenes_validacion.astype('float32')
+        self.imagenes_aprendizaje /= 255
+        self.imagenes_validacion /= 255
+
+        self.etiquetas_aprendizaje = keras.utils.to_categorical(self.etiquetas_aprendizaje, cantidad_de_clases)
+        self.etiquetas_validacion = keras.utils.to_categorical(self.etiquetas_validacion, cantidad_de_clases)
+
+        return self.imagenes_aprendizaje, self.imagenes_validacion, self.etiquetas_aprendizaje, self.etiquetas_validacion
+
+class redNeuronal():
+    def __init__(self):
+        self.redCNN = Sequential()
+
+    def red(self, anchoimagen, largoimagen, cantidad_de_clases):
+        self.redCNN.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(anchoimagen, largoimagen, 1)))
+        self.redCNN.add(Conv2D(64, (3, 3), activation='relu'))
+        self.redCNN.add(MaxPooling2D(pool_size=(2, 2)))
+        self.redCNN.add(Dropout(0.25))
+        self.redCNN.add(Flatten())
+        self.redCNN.add(Dense(128, activation='relu'))
+        self.redCNN.add(Dropout(0.5))
+        self.redCNN.add(Dense(cantidad_de_clases, activation='softmax'))
+
+        return self.redCNN
+
+    def compilacion(self, redCNN):
+        redCNN.compile(loss=keras.losses.categorical_crossentropy,
+                        optimizer=keras.optimizers.Adadelta(),
+                        metrics=['accuracy'])
+
+        return redCNN
+
+    def entrenamiento(self, redCNN, imagenes_aprendizaje, etiquetas_aprendizaje, imagenes_validacion, etiquetas_validacion, epochs, batch_size):
+        redCNN.fit(imagenes_aprendizaje, etiquetas_aprendizaje,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(imagenes_validacion, etiquetas_validacion))
+
+        return redCNN
+
+    def guardado(self, redCNN):
+        redCNN.save('modelo/modelo.h5')
+
+
+    def evaluacion(self, redCNN, imagenes_validacion, etiquetas_validacion):
+        score = redCNN.evaluate(imagenes_validacion, etiquetas_validacion, verbose=0)
+        print('Pérdida:', score[0])
+        print('Precisión:', score[1])
 
 
 def main():
